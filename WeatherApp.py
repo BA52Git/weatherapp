1e2f80842f39e9100196e69ea2609a3f87fc7f5b
BA52Git <benjaminavery@gmail.com>
2021-02-20 21:11:37 -0800

    Functional code, right now just short circuits to printing today's weather in a nice print message "It is [temperature] degrees and the weather is [shortForecast"

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,42 +1,48 @@
 #Author: Ben Avery
+#Date: 2-20-21
 
-import json, urllib, urllib.request, datetime
+import json, urllib, urllib.request, time, logging
 
 #"""
-#//a "debugmode" variable, when true checks to see if dummy json data is available.  If yes, load it, if no download the data and save it to disk.
+#//a "debugmode" variable, when true checks to see if dummy json data is available.
+# #   If yes, load it, if no download the data and save it to disk.
 
 
 #"""
 def gettodaysforecast():
-    curtime = datetime.time()
-    if 3 < curtime.tm_hour < 15:
-        curtime.tm_hour = 6
-    else: curtime.tm_hour = 18
-    rightnow = "{}-0{}-{}T{}:00:00-08:00".format(curTime.tm_year, curTime.tm_mon, curTime.tm_mday, curTime.tm_hour)
-    todayforecast = getcurweather(('48.769', '-122.459'))
+    curTime = time.localtime()
+    if 3 < curTime.tm_hour < 15:
+        nearesthour = 6
+    else: nearesthour = 18
+    rightnow = "{}-0{}-{}T{}:00:00-08:00".format(curTime.tm_year, curTime.tm_mon, curTime.tm_mday, nearesthour)
+    defaultlocation =  json.loads('{ "long" : 48.769 , "lat" : -122.459}')
+    todayforecast = getcurweather(defaultlocation)
+
+
+    #TODO turn this into a function that takes in today forecast and returns the URL
+    forecastRequest = urllib.request.urlopen(todayforecast["properties"]['forecast'])
+    forecastData = json.load(forecastRequest)
+    shortforecast = forecastData['properties']['periods'][0]['shortForecast']
+    temperature = forecastData['properties']['periods'][0]['temperature']
+    print("It is {} degrees and the weather is {}".format(temperature,shortforecast))
+
 
 def getcurweather(address):
     weburl = "https://api.weather.gov/points/{},{}".format(address["long"], address["lat"])
-    print(weburl)
+    #print(weburl)
     currequest = urllib.request.urlopen(weburl)
-    curdata = json.loads(currequest)
-    print(curdata)
+#    print(currequest)
+    curdata = json.load(currequest)
+    logging.debug(curdata)
     return curdata
 
 #...
 
 def main():
+    logging.basicConfig(level = logging.DEBUG, format= '%(asctime)s - %(levelname)s - %(message)s')
     #open the url (to an HTTPResponse object), load the data into a json object.
     defaultcoords = '{ "long" : 48.769 , "lat" : -122.459}'
     coordjson = json.loads(defaultcoords)
-    data = getcurweather(coordjson)
-    # request = urllib.request.urlopen("https://api.weather.gov/points/48.76977025604445,-122.45939985854866")
-    # data = json.load(request)
-
-    #open the forecast url from the previous webpage and load it into the forecastData json object for dissection
-    forecastRequest = urllib.request.urlopen(data["properties"]['forecast'])
-    forecastData = json.load(forecastRequest)
-    print(forecastData['properties']['periods'][0]['shortForecast'])
-
+    data = gettodaysforecast()
 
 main()
